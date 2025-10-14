2db803128dcf9b37842d87549752c4e83a9e9f7a
Mina R <minarumang@icloud.com>
2025-10-14 16:35:31 +0200

    Add basic arithmetic operations

--- script/index.js
+++ script/index.js
@@ -4,3 +4,11 @@ let name = "Mina";
 let color = "Purple";
 
 console.log(`Hello im ${name}. Im ${age} years old and my favourite color is ${color}.`);
+
+let num1 = 6;
+let num2 = 4;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
