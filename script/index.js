20a796d40ee929c1a9f05f1c0c9353eee759ec07
Mina R <minarumang@icloud.com>
2025-10-20 14:41:23 +0200

    Conditional Statements and Functions.

--- script/index.js
+++ script/index.js
@@ -14,16 +14,30 @@
 //console.log(num1 / num2);
 
 //1. Ask the user to enter a price tag using prompt(), for example: $49.99
-let userInput = prompt("Enter a price tag:");
+//let userInput = prompt('Enter a price tag:');
 
 //2. Remove the dollar sign
-let priceNoSign = userInput.replace('$', '');
+//let priceNoSign = userInput.replace('$', '');
 
 //3. Convert the input from string to number 
-priceNoSign = Number(priceNoSign)
+//priceNoSign = Number(priceNoSign)
 
 //4. Reduce the price by 10 percent 
-priceNoSign = 0.9 * priceNoSign;
+//priceNoSign = 0.9 * priceNoSign;
 
 //5. Print out the new price using console.log(), for example: 'Your new price is: $44.99' 
-console.log("Your new price is: $" + priceNoSign); 
+//console.log("Your new price is: $" + priceNoSign); 
+
+function isValidPassword(password, username) {
+    if (password.length < 8) {
+        return false;
+}
+if (password.includes(' ')){
+    return false;
+}
+if (password.includes(username)) {
+    return false;
+}
+return true;
+}
+console.log(isValidPassword("supersecurepassword123abclol", "Mina"));
